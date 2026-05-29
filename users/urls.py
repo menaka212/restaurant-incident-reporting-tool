e14123ba6f3aef663004da3546f65989bf938a6e
menaka212 <menakamanavalan2@gmail.com>
2026-05-29 15:26:48 +0530

    Update check users

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('check-users/', views.check_users, name='check_users'),
     path('create-admin/', views.create_admin, name='create_admin'), 
+    path('create-users/', views.create_users, name='create_users'),
 ]
