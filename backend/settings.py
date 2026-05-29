feabe52cbd0ab98b791f3143243873ec76199940
menaka212 <menakamanavalan2@gmail.com>
2026-05-29 12:46:42 +0530

    Fixed settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e#dfd*2bh-5+kafje5-*a*hxhxayv0_kx5^t08$)ne&=us4szh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 import dj_database_url
-import os
+
 ALLOWED_HOSTS = ['*']
 
 
